1b4a0c88c4dacd3b736391bf39b30da95c9c339a
saraivas <felipesaraivadossantos@gmail.com>
2022-07-28 00:15:28 -0300

    exercicio 1 part 1 e 2

---
+++ semana 1/ex1.js
@@ -0,0 +1,27 @@
+/*
+1 - Crie variáveis para armazenar algumas informações de cadastro de
+um estudante,são elas: nome, sobrenome, dia do nascimento, mês do
+nascimento, ano donascimento, primeira nota e segunda nota.
+A partir dessas informações, você devemostrar um relatório na tela
+da seguinte forma: Nome Completo, Idade, Data de Nascimento e Média:
+
+Exemplo de Saída: Nome Completo: João Pedro Bretanha
+                  Idade: 29
+                  Data de Nascimento: 11/04/1992
+                  Média: 8  */
+
+let estudante = {nome:'Felipe',
+                 sobrenome:'Saraiva',
+                 idade:23,
+                 diaNascimento:'07',
+                 mesNascimento:'06',
+                 anoNascimento:'99',
+                 notas:[7, 8.5]};
+
+console.log(`Nome Completo: ${estudante.nome} ${estudante.sobrenome}\nIdade: ${estudante.idade}\nData de Nascimento: ${estudante.diaNascimento}/${estudante.mesNascimento}/${estudante.anoNascimento}\nMédia: ${(estudante.notas[0]+estudante.notas[1])/2}`)
+
+/*2 - Crie um programa que some os números 5, 10 e 15. Salve o resultado em umavariável e imprima no console.*/
+let numeros = [5, 10, 15];
+let soma = 0;
+soma = numeros[0]+numeros[1]+numeros[2];
+console.log('\n'+soma)
